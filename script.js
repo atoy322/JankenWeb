2b5e4dbd4532b1aa7809bca7fe97f76db1756b53
atoy322 <atoy_0322@yahoo.co.jp>
2021-12-06 16:42:54 +0900

    edit some files

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const kachi = {gu: "pa", choki: "gu", pa: "choki"};
+
 function Startup() {
     var gu_button = document.getElementById("gu");
     var choki_button = document.getElementById("choki");
@@ -8,5 +10,27 @@ function Startup() {
 }
 
 function onClick(event) {
-    alert(event.target.tagName);
+    var com_img = document.getElementById("COM_img");
+
+    var button_gu = document.getElementById("gu");
+    var button_choki = document.getElementById("choki");
+    var button_pa = document.getElementById("pa");
+
+    shape = event.target.name;
+    console.log(shape);
+
+    if(shape == "gu") {
+        button_choki.style.backgroundColor = "";
+        button_pa.style.backgroundColor = "";
+    } else if(shape == "choki") {
+        button_gu.style.backgroundColor = "";
+        button_pa.style.backgroundColor = "";
+    } else if(shape == "pa") {
+        button_gu.style.backgroundColor = "";
+        button_choki.style.backgroundColor = "";
+    }
+
+    var button = document.getElementById(shape);
+    button.style.backgroundColor = "#ebd694";
+    com_img.src = `resources/janken_${kachi[shape]}.png`;
 }
