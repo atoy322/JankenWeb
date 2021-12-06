f4f39fe2626b000da4d5cbf816b35f2de66e6aef
atoy322 <atoy_0322@yahoo.co.jp>
2021-12-06 19:04:50 +0900

    update

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const kachi = {gu: "pa", choki: "gu", pa: "choki"};
+var opacity = 0;
+var scale = 0;
+var counter = 0;
 
 function Startup() {
     var gu_button = document.getElementById("gu");
@@ -7,14 +10,15 @@ function Startup() {
     gu_button.addEventListener("click", onClick);
     choki_button.addEventListener("click", onClick);
     pa_button.addEventListener("click", onClick);
+
+    setInterval(updateImage, 10);
 }
 
 function onClick(event) {
-    var com_img = document.getElementById("COM_img");
-
     var button_gu = document.getElementById("gu");
     var button_choki = document.getElementById("choki");
     var button_pa = document.getElementById("pa");
+    var msg = document.getElementById("counter");
 
     shape = event.target.name;
     console.log(shape);
@@ -32,5 +36,27 @@ function onClick(event) {
 
     var button = document.getElementById(shape);
     button.style.backgroundColor = "#ebd694";
+
+    startAnimation(shape);
+
+    counter += 1;
+    msg.innerHTML = `コンピュータ: ${counter} 勝 0 敗 / あなた: 0 勝 ${counter} 敗 / <strong class="red">あなたの勝率 0 [%]</strong>`;
+}
+
+function startAnimation(shape) {
+    var com_img = document.getElementById("COM_img");
+    opacity = 0;
+    scale = 0;
     com_img.src = `resources/janken_${kachi[shape]}.png`;
+}
+
+function updateImage() {
+    var com_img = document.getElementById("COM_img");
+    com_img.style.opacity = `${opacity}`;
+    com_img.style.transform = `scale(${scale}, ${scale})`;
+
+    if(opacity < 1 || scale < 1) {
+        opacity += 0.01;
+        scale += 0.01;
+    }
 }
